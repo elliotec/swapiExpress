a03e46be4b4eb09a5933f7defdf497659760a19a
Mike Elliott <melliott@overstock.com>
2017-06-23 16:04:17 -0600

    requirements complete

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const request = require('request');
 const fetch = require('node-fetch');
 const app = express();
 const swapi = 'http://swapi.co/api';
@@ -7,8 +6,8 @@ const swapi = 'http://swapi.co/api';
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index', {heading: "hello world"});
-})
+    res.render('index', {heading: 'Hello Grow!'});
+});
 
 app.get('/character/:name', function (req, res) {
     const url = `${swapi}/people/?search=${req.params.name}`;
@@ -29,8 +28,7 @@ app.get('/character/:name', function (req, res) {
             homeworld: character.homeworld
         });
     }
-
-})
+});
 
 app.get('/characters', function (req, res) {
     let mappedCharacters = [];
@@ -63,7 +61,7 @@ app.get('/characters', function (req, res) {
 
         return fetch(url)
             .then(res => res.json())
-            .then(json => json.results)
+            .then(json => json.results);
     }
 
 
@@ -92,13 +90,45 @@ app.get('/characters', function (req, res) {
     Promise.all(allPages).then(pages => {
         const allOrSortedCharacters = maybeSortCharacters(pages, sortValue);
 
-        res.send(allOrSortedCharacters)
+        res.send(allOrSortedCharacters);
     });
-})
+});
 
 app.get('/planetresidents', function (req, res) {
-    res.render('index', {heading: "hello world"});
-})
+    const url = `${swapi}/planets/`;
+
+    function mapResidentsToPlanets(json) {
+        const planets = json.results;
+        return Promise.all(planets.map(planet => {
+            return getResidents(planet).then(residents => {
+                return { [planet.name] :residents};
+            });
+        }));
+    }
+
+    function getResidents(planet) {
+        const residentsUrls = planet.residents;
+        return Promise.all(residentsUrls.map(url =>
+            fetch(url)
+                .then(res => res.json())
+                .then(resident => resident.name)
+            )
+        );
+    }
+
+    function buildPlanetsResidentsObject(array){
+        return array.reduce((object, item) => {
+            return Object.assign(object, item);
+        }, {});
+    }
+
+    fetch(url)
+        .then(res => res.json())
+        .then(json => mapResidentsToPlanets(json))
+        .then(array => buildPlanetsResidentsObject(array))
+        .then(result => res.send(result));
+
+});
 
 app.listen(3000, () => {
     console.log('express app listening at localhost:3000');
